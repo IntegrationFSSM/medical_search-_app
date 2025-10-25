905cf9893c63222faede0eebba8fcbbc8e59fa82
EnnhiliYassine <yassinenhili2002@gmail.com>
2025-10-25 19:10:52 +0100

    Improve consultation report: remove similarity score, clean markdown from treatment plan, remove generation date

--- pathology_search/templatetags/custom_filters.py
+++ pathology_search/templatetags/custom_filters.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.filter(name='clean_text')
 def clean_text(value):
     """
-    Nettoie le texte en enlevant les crochets, guillemets, et autres caractères indésirables.
+    Nettoie le texte en enlevant les crochets, guillemets, markdown et autres caractères indésirables.
     """
     if not value:
         return value
@@ -24,8 +24,23 @@ def clean_text(value):
     text = text.replace('["', '').replace('"]', '')
     text = text.replace("['", '').replace("']", '')
     
-    # Nettoyer les espaces multiples
-    text = re.sub(r'\s+', ' ', text)
+    # Enlever les markdown
+    # Enlever les titres markdown (## Titre)
+    text = re.sub(r'#{1,6}\s*', '', text)
+    
+    # Enlever le gras markdown (**texte** ou __texte__)
+    text = re.sub(r'\*\*([^\*]+)\*\*', r'\1', text)
+    text = re.sub(r'__([^_]+)__', r'\1', text)
+    
+    # Enlever l'italique markdown (*texte* ou _texte_)
+    text = re.sub(r'\*([^\*]+)\*', r'\1', text)
+    text = re.sub(r'_([^_]+)_', r'\1', text)
+    
+    # Enlever les liens markdown [texte](url)
+    text = re.sub(r'\[([^\]]+)\]\([^\)]+\)', r'\1', text)
+    
+    # Nettoyer les espaces multiples mais garder les sauts de ligne
+    text = re.sub(r' +', ' ', text)
     
     return text.strip()
 
